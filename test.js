4bd19ca915bde76c4cede1f6270ba0421111f4fb
kai.zhu <kai.zhu@windebt.com>
2019-10-18 13:25:03 -0500

    inline.jws.sign

--- test.js
+++ test.js
@@ -498,7 +498,7 @@ gtudSqQGI0ogrSbNb6UIxUILysbRmFN8d25kszDukf0KkssHGpuU8orfknxC8RoL
 Pk+DWW+eF1neo8zw7kAkMW0QBnVEYAVYcqxSX42Osl2d0l/KaskavT06unvCzjiR
 CwIDAQAB
 -----END PUBLIC KEY-----`));
-    tokenDecrypted = require("./lib/jws/sign").sign2({
+    tokenDecrypted = require("./lib/jws/sign")({
         "urn:example:claim": "foo"
     }, local.keyPrivate, {
         algorithm: "PS256",
